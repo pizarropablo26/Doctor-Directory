fb2ee31514fd5e2b0c07bf6f77886232399660f3
J J <76414680+pizarropablo26@users.noreply.github.com>
2022-11-29 02:05:01 -0500

    Add files via upload

---
+++ CS3319/phpworkshop/getpets.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dr. Western's Vet Clinic-Your Pets</title>
+</head>
+<body>
+<?php
+include 'connectdb.php';
+?>
+<h1>Here are your pets:</h1>
+<ol>
+<?php
+   $whichOwner= $_POST["petowners"];
+   $query = 'SELECT * FROM owner, pet WHERE pet.ownerid=owner.ownerid AND pet.ownerid="' . $whichOwner . '"';
+   $result=mysqli_query($connection,$query);
+    if (!$result) {
+         die("database query2 failed.");
+     }
+    while ($row=mysqli_fetch_assoc($result)) {
+        echo '<li>';
+        echo $row["petname"];
+	echo '<img src="' . $row["petpicture"] . '" height="60" width="60">';
+     }
+     mysqli_free_result($result);
+?>
+</ol>
+<?php
+   mysqli_close($connection);
+?>
+</body>
+</html>
